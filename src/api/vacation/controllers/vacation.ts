b50e214931d8a4bef73b5f25968f8a662dcfb1a7
Rémi PONCHE <ponche62880@gmail.com>
2023-07-28 00:03:18 +0200

    👔 Création des routes API

---
+++ src/api/vacation/controllers/vacation.ts
@@ -0,0 +1,7 @@
+/**
+ * vacation controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::vacation.vacation');
---
+++ src/api/vacation/routes/vacation.ts
@@ -0,0 +1,7 @@
+/**
+ * vacation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::vacation.vacation');
